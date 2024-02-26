b18f17691211715100567200a804de2af0b282ce
Marcelo-Nobre <joaomarceloviana6@gmail.com>
2024-02-26 17:46:32 -0300

    feat: Adicionando as Keys ao projeto

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -23,14 +23,17 @@ export function Post({ author, publishedAt, content }) {
   })
 
   function handleCreateNewComment() {
-    //pra página não recarregar
-    event.preventDefault()
-    //pega o valor do textarea
-    const newCommentText = event.target.comment.value
-    //seta um novo valor e salva ele na váriavel comments ...comments é pra armazenar todos os valores anterires
-    setComments([...comments, newCommentText]);
-    //limpa o input
-    setNewCommentText('');
+    if (event.target.comment.value == '') {
+      return
+    }
+      //pra página não recarregar
+      event.preventDefault()
+      //pega o valor do textarea
+      const newCommentText = event.target.comment.value
+      //seta um novo valor e salva ele na váriavel comments ...comments é pra armazenar todos os valores anterires
+      setComments([...comments, newCommentText]);
+      //limpa o input
+      setNewCommentText('');
   }
 
   //essa função faz o textarea escutar/refletir o que está acontecendo
@@ -61,9 +64,9 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
         {content.map(line => {
           if (line.type == 'paragraph') {
-            return <p>{line.content}</p>
+            return <p key={line.content}>{line.content}</p>
           } else if (line.type == 'link') {
-            return <p><a href="">{line.content}</a></p>
+            return <p key={line.content}><a href="">{line.content}</a></p>
           }
         })}
       </div>
@@ -84,7 +87,7 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment content={comment} />
+          return <Comment key={comment} content={comment} />
         })}
       </div>
     </article>
